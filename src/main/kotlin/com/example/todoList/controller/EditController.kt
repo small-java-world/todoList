06df259225572c050ac78ada02b968d3b56dc044
Kawano Fumihiko <kawano.fumihiko@gmail.com>
2021-04-30 15:22:24 +0900

    細かい不具合を修正

--- src/main/kotlin/com/example/todoList/controller/EditController.kt
+++ src/main/kotlin/com/example/todoList/controller/EditController.kt
@@ -5,25 +5,28 @@ import com.example.todoList.service.TodoService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.validation.BindingResult
-import org.springframework.web.bind.annotation.*
-import org.springframework.web.bind.support.SessionStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.servlet.ModelAndView
-import javax.validation.Valid
-
 
 @Controller
 class EditController @Autowired constructor(private val todoService: TodoService) {
     @GetMapping("/inputForm")
     fun inputForm(@ModelAttribute todo: Todo): ModelAndView =
-        ModelAndView("/inputForm").apply { addObject("todo",
-            todo) }
+        ModelAndView("/inputForm").apply {
+            addObject(
+                "todo",
+                todo
+            )
+        }
 
     @PostMapping("/save")
-    fun save(@ModelAttribute @Valid todo:Todo, bindingResult: BindingResult,
-               model: Model, sessionStatus: SessionStatus
+    fun save(
+        @ModelAttribute todo: Todo,
+        model: Model
     ): String {
-        return if(todoService.save(todo)) {
+        return if (todoService.save(todo)) {
             "redirect:/top/list";
         } else {
             model.addAttribute("errorMessage", "登録失敗")
